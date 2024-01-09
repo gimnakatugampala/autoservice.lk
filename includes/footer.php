0897e7f36d7d35800d2995f0e957165a564d586f
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-01-09 21:39:52 +0530

    2024_1_9_other

--- includes/footer.php
+++ includes/footer.php
@@ -19,3 +19,23 @@
 <script src="../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
+
+
+<!-- Page specific script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
--- includes/sub-footer.php
+++ includes/sub-footer.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.1.0
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2024 <a href="https://autoservice.lk">autoservice.lk</a>.</strong> All rights reserved.
   </footer>
