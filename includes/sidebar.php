2a85f5b47c00f0ee29fbccea688fb302ae497b7e
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-01-10 07:50:27 +0530

    2024_1_10_employee

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -107,6 +107,30 @@
             </ul>
           </li>
 
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Manage Employees
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../employees/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Employee List</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../employees/add-employee.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Add Employee</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
 
           <li class="nav-item">
             <a href="#" class="nav-link">
