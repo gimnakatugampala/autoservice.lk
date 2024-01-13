fd56d2ee5589f467e0cadea2da2ad0b6b3ad7f07
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-01-13 13:07:59 +0530

    2024_1_13_maintaince

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -159,7 +159,31 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Manage Services
+                 Maintenance Package
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../maintenance/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Maintenance List</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../maintenance/add-maintenance.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Add Maintenance</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Manage Order Services
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
