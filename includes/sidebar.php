27527f293fd9346c75f419338b293e433a87ca03
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-01-19 11:21:25 +0530

    2024_1_19_washers

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -189,13 +189,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="../repair/" class="nav-link">
+                <a href="../washer/" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Washers List</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../repair/add-repair.php" class="nav-link">
+                <a href="../washer/add-washer.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Add Washer</p>
                 </a>
---
+++ washer/add-washer.php
@@ -0,0 +1,110 @@
+
+<?php include_once '../includes/header.php';?>
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <?php include_once '../includes/loader.php';?>
+
+  <!-- Navbar -->
+  <?php include_once '../includes/navbar.php'; ?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+ <?php include_once '../includes/sidebar.php';?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Add Washer</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Manage Washer</a></li>
+              <li class="breadcrumb-item active">Add Washer</li>
+            </ol>
+          </div>
+
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+      
+            <!-- /.card -->
+            <div class="card">
+              <div class="card-body">
+
+              <div class="row">
+
+                <div class="col-md-6">
+                    
+                          <div class="form-group">
+                                <label>Vehicle Type <span class="text-danger">*</span></label>
+                                <select class="custom-select">
+                                <option>option 1</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                      </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                          <label for="exampleInputEmail1">Price (LKR) <span class="text-danger">*</span></label>
+                          <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Price">
+                      </div>
+                </div>
+
+              
+
+      
+
+                <div class="col-md-12">
+                <button type="button" class="btn bg-gradient-secondary">Cancel</button>
+                <button type="button" class="btn bg-gradient-primary">Submit</button>
+                </div>
+
+              </div>
+
+
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php include_once '../includes/sub-footer.php';?>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<?php include_once '../includes/footer.php';?>
+
+</body>
+</html>
