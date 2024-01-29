cdef26dba869b969c8a355f5d9a3a789e77bdbe1
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-01-29 09:25:34 +0530

    2024_1_29_station_reg

--- api/station_register.php
+++ api/station_register.php
@@ -1,9 +1,60 @@
 <?php
 
+session_start();
+
 require_once '../includes/db_config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    $station_code = $_POST['station_code'];
+    $station_name = $_POST['station_name'];
+    $email =  $_POST['email'];
+    $password = $_POST['password'];
+    $latitude = $_POST['lat'];
+    $long = $_POST['long'];
     
+    $hashed_password = sha1($password);
+
+    $sql = "SELECT * FROM service_station WHERE email = '$email' OR service_name = '$station_name'";
+    $result = $conn->query($sql);
+
+    $dateNow =date("Y-m-d H:i:s");
+
+    if ($result->num_rows > 0) {
+        echo "User Exist";
+        // echo $station_logo;
+        
+    } else {
+        // echo "User Does Not Exist";
+
+       
+        // echo $station_logo;
+     
+
+        // Save Data
+        $sql = "INSERT INTO service_station (code, service_name,email,password,created_date,is_deleted,is_active,
+        latitude,`long`) 
+        VALUES ('$station_code', '$station_name', '$email','$hashed_password','$dateNow',0,1,'$latitude','$long')";
+        if ($conn->query($sql) === TRUE) {
+
+            $sql = "SELECT * FROM service_station WHERE email = '$email' OR service_name = '$station_name'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // SESSIONS
+                 $row = $result->fetch_assoc();
+                 $_SESSION["station_id"] = $row["id"];
+            }
+        
+            echo "success";
+        }else{
+            echo $conn->error;
+        }
+
+    }
+
+
 }
 
 
--- assets/js/station_register.js
+++ assets/js/station_register.js
@@ -6,6 +6,11 @@ $(document).ready(function () {
         var email = $("#email").val();
         var password = $("#password").val();
         var con_password = $("#con_password").val();
+        var lat = ""
+        var long = ""
+
+        console.log(station_logo)
+        
 
         if(station_name == ""){
              Swal.fire({
@@ -60,7 +65,81 @@ $(document).ready(function () {
 
               return
 
+        }else if(con_password != password){
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Passwords Do not Match",
+              });
+
+              return
+
         }
+
+    
+
+        fetch('https://ipapi.co/json/')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            // console.log('IP API Response:', data);
+            lat = data.latitude
+            long = data.longitude
+        })
+        .then(() => {
+
+    console.log(generateUUID())
+    console.log(station_name)
+    console.log(email)
+    console.log((lat).toString())
+
+        //  SAVE DATA
+        $.ajax({
+            type: "POST",
+            url: "../api/station_register.php",
+            data: {
+              station_code:generateUUID(),
+              station_name,
+              email:email,
+              password:con_password,
+              lat:(lat).toString(),
+              long:(long).toString(),
+            },
+            success: function (response) {
+     
+                console.log(response)
+
+              if (response === "success") {
+                window.location.href = "../auth/user-register.php";
+                // console.log("Success")
+    
+              } else if (response == "User Exist"){
+                Swal.fire({
+                    icon: "error",
+                    title: "Login failed",
+                    text: "Service Station Already Exist.",
+                  });
+
+              }else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Login failed",
+                    text: "Please check your credentials.",
+                  });
+              }
+
+            },
+            error:function (error) {
+                console.log(error)
+            }
+          });
+
+        })
+
+
+
+        
 
         // if (station_logo.files.length > 0) {
         //     var selectedFile = station_logo.files[0];
@@ -73,9 +152,13 @@ $(document).ready(function () {
         // console.log(station_logo)
         // console.log(email)
         // console.log(password)
-        console.log(con_password)
+        // console.log(con_password)
 
-    })
+             
+         })
+
+
+      
 
     // Swal.fire({
     //     icon: "error",
