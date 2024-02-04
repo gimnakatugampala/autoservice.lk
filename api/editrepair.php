cf82c34084e8ec697623acaf5baaab9b0b56b069
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-02-04 23:32:51 +0530

    2024_2_4_edit_repair

---
+++ api/editrepair.php
@@ -0,0 +1,50 @@
+
+<?php
+
+session_start();
+
+require_once '../includes/db_config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id = $_POST['id'];
+    $repair_name = $_POST['repair_name'];
+    $data = json_decode($_POST['manage_repair'], true);
+
+
+        // Save Data
+        $sql = "UPDATE repair SET 
+        name = '$repair_name'
+        WHERE id = '$id'";
+        if ($conn->query($sql) == TRUE) {
+        //     $repairPackageID = $conn->insert_id;
+
+            // Manage Repair
+            foreach ($data as $row) {
+                $repairID = $row['repairID'];
+                $vehicleClassID = $row['ID'];
+                $price = $row['price'];
+
+            $manageRepairSQL = "UPDATE manage_repair SET 
+            price = '$price'
+            WHERE repair_id = '$repairID' AND vehicle_class_id = '$vehicleClassID'";
+            if ($conn->query($manageRepairSQL) !== true) {
+                echo 'Error: ' . $manageRepairSQL . '<br>' . $conn->error;
+                exit();
+            }
+        }
+
+        echo "success";
+
+        }else{
+            echo $conn->error;
+        }
+
+  
+
+        
+
+}
+
+
+?>
--- assets/js/getrepairs.js
+++ assets/js/getrepairs.js
@@ -8,27 +8,20 @@ $(document).ready(function () {
 
     var tbbody = $("#tbeditrepair");
     var items = [];
-
-    // get All vehicle Classes
-    $.ajax({
-        type: "POST",
-        url: "../api/getvehicleclassdata.php",
-        success: function (data) {
-            // console.log(JSON.parse(data))
-            // populateTableRepair(JSON.parse(data))
-     }
-    })
+   
 
     function populateTableRepair(data) {
         data.forEach(function (list) {
-            console.log(list)
+            // console.log(list)
             var row = $("<tr>");
+            row.append("<td class='repairID' style='display:none;'>" + list.repair_id + "</td>");
             row.append("<td class='rowID' style='display:none;'>" + list.id + "</td>");
             row.append(`<td>${list.name}</td>`);
             row.append(`<td><input value="${list.price}" type="text" class="form-control price" placeholder="LKR"></td>`);
             tbbody.append(row);
 
             var item = {
+                repairID:row.find(".repairID")[0],
                 rowID:row.find(".rowID")[0],
                 priceInput: row.find(".price")[0],
             }
@@ -50,7 +43,6 @@ $(document).ready(function () {
            console.log(data);
    
            let RepairNameElement = document.getElementById("repair_name");
-    
            var buttonElement = document.getElementById("btn_update_repair");
         
            buttonElement.setAttribute("data-id", data.data_content[0].id);
@@ -63,4 +55,70 @@ $(document).ready(function () {
        });
    
      }
+
+
+     $("#btn_update_repair").click(function () {
+        var data = [];
+
+        var dataIdValue = $(this).data("id");
+        $(".price").each(function () {
+            var ID = $(this).closest("tr").find(".rowID").text();
+            var repairID = $(this).closest("tr").find(".repairID").text();
+            var price = $(this).closest("tr").find(".price").val();
+
+            data.push({
+                price: price == "" ? 0 : price,
+                ID:ID,
+                repairID
+              });
+        })
+
+        console.log(data)
+        console.log(dataIdValue)
+       
+        var repair_name = $("#repair_name").val();
+   
+
+        if(repair_name == ""){
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Please Enter Repair Name",
+              });
+              return
+        }else{
+
+            //  SAVE DATA
+            $.ajax({
+                type: "POST",
+                url: "../api/editrepair.php",
+                data: {
+                    id:dataIdValue,
+                    repair_name:repair_name,
+                    manage_repair:JSON.stringify(data)
+                },
+                success: function (response) {
+        
+                    console.log(response)
+
+                // if (response === "success") {
+                //     window.location.href = "../repair/";
+                //     // console.log("Success")
+        
+                // }else {
+                //     Swal.fire({
+                //         icon: "error",
+                //         title: "Please Try Again",
+                //         text: "Something Went Wrong",
+                //     });
+                // }
+
+                },
+                error:function (error) {
+                    console.log(error)
+                }
+            });
+        }
+             
+         })
 })
