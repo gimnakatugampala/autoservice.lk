92be3cb12fc53427c4e9fa2b3dd3e30b2a2a1f0e
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-02-04 11:36:38 +0530

    2024_2_4_vehicles

---
+++ api/editvehicle.php
@@ -0,0 +1,58 @@
+<?php
+
+session_start();
+
+require_once '../includes/db_config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id = $_POST['id'];
+    $vehicle_number = $_POST['vehicle_number'];
+    $engine_number = $_POST['engine_number'];
+    $vehicleclass = $_POST['vehicleclass'];
+    $vehiclemanufacturer = $_POST['vehiclemanufacturer'];
+    $vehiclecountry = $_POST['vehiclecountry'];
+    $vehiclemodel = $_POST['vehiclemodel'];
+    $vehiclefueltype = $_POST['vehiclefueltype'];
+    $vehicleowner = $_POST['vehicleowner'];
+    $vehicleyear = $_POST['vehicleyear'];
+    $chassis_number = $_POST['chassis_number'];
+    $vehicle_color = $_POST['vehicle_color'];
+
+    $sql = "SELECT * FROM vehicle WHERE vehicle_number = '$vehicle_number' AND id != '$id'";
+    $result = $conn->query($sql);
+
+
+    if ($result->num_rows > 0) {
+        echo "Vehicle Exist";        
+    } else {
+        // echo "Vehicle Does Not Exist";
+        // Save Data
+        $sql = "UPDATE vehicle SET 
+        vehicle_number = '$vehicle_number',
+        vehicle_color = '$vehicle_color',
+        engine_number = '$engine_number',
+        chassis_number = '$chassis_number',
+        vehicle_owner_id = '$vehicleowner',
+        vehicle_model_id = '$vehiclemodel',
+        vehicle_year_manufacturer_id = '$vehicleyear',
+        vehicle_fuel_type_id = '$vehiclefueltype',
+        vehicle_country_id = '$vehiclecountry',
+        vehicle_manufacturer_id = '$vehiclemanufacturer',
+        vehicle_class_id = '$vehicleclass'
+        WHERE id = '$id'
+        ";
+        if ($conn->query($sql) === TRUE) {
+
+            echo "success";
+        }else{
+            echo $conn->error;
+        }
+
+    }
+
+
+}
+
+
+?>
--- assets/js/getvehicles.js
+++ assets/js/getvehicles.js
@@ -6,6 +6,105 @@ $(document).ready(function () {
 
     document.addEventListener('DOMContentLoaded', getVehicles());
 
+    $("#btn_edit_vehicle").click(function () {
+  
+      var vehicle_number = $("#vehicle_number").val();
+      var engine_number = $("#engine_number").val();
+      var cmbvehicleclass = $("#cmbvehicleclass").val();
+      var vehicle_img = $("#vehicle_img").val();
+      var cmbvehiclemanufacturer = $("#cmbvehiclemanufacturer").val();
+      var cmbvehiclecountry = $("#cmbvehiclecountry").val();
+      var cmbvehiclemodel = $("#cmbvehiclemodel").val();
+      var cmbvehiclefueltype = $("#cmbvehiclefueltype").val();
+      var cmbvehicleowner = $("#cmbvehicleowner").val();
+      var cmbvehicleyear = $("#cmbvehicleyear").val();
+      var chassis_number = $("#chassis_number").val();
+      var vehicle_color = $("#vehicle_color").val();
+      var dataIdValue = $(this).data("id");
+
+      if(vehicle_number == ""){
+          Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Please Enter Vehicle Number.",
+          });
+          return
+      }else if(engine_number == ""){
+          Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Please Enter Enter Engine Number.",
+          });
+          return
+      }else if(chassis_number == ""){
+          Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Please Enter Enter Chassis Number.",
+          });
+          return
+      }else if(vehicle_color == ""){
+          Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Please Enter Enter Vehicle Color.",
+          });
+          return
+      }else{
+
+
+          //  SAVE DATA
+          $.ajax({
+              type: "POST",
+              url: "../api/editvehicle.php",
+              data: {
+              id:dataIdValue,
+              vehicle_number:vehicle_number,
+              engine_number:engine_number,
+              vehicleclass:cmbvehicleclass,
+              vehiclemanufacturer:cmbvehiclemanufacturer,
+              vehiclecountry:cmbvehiclecountry,
+              vehiclemodel:cmbvehiclemodel,
+              vehiclefueltype:cmbvehiclefueltype,
+              vehicleowner:cmbvehicleowner,
+              vehicleyear:cmbvehicleyear,
+              chassis_number:chassis_number,
+              vehicle_color:vehicle_color
+              },
+              success: function (response) {
+      
+                  console.log(response)
+
+                if (response === "success") {
+
+                  window.location.href = "../vehicles/";
+      
+                }else if(response == "Vehicle Exist"){
+
+                  Swal.fire({
+                      icon: "error",
+                      title: "Error",
+                      text: "Vehicle Already Exist.",
+                    });
+
+                }else {
+                  Swal.fire({
+                      icon: "error",
+                      title: "Something Went Wrong",
+                      text: "Please Try Again.",
+                    });
+                }
+
+              },
+              error:function (error) {
+                  console.log(error)
+              }
+          });
+
+      }
+
+  })
+
 
    function getVehicles(){
        $.ajax({
@@ -25,20 +124,20 @@ $(document).ready(function () {
            let VehicleOwnerElement = document.getElementById("cmbvehicleowner");
            let VehicleYearElement = document.getElementById("cmbvehicleyear");
            let ChassisNumberElement = document.getElementById("chassis_number");
-           let VehicleColorElement = document.getElementById("vehicle_color");
-
-       
-
-        VehicleNumberElement.value = data.data_content[0].vehicle_number
-        EngineNumberElement.value = data.data_content[0].engine_number
-        VehicleManufacturerElement.value = data.data_content[0].vehicle_manufacturer_id
-        VehicleCountryElement.value = data.data_content[0].vehicle_country_id
-        VehicleModelElement.value = data.data_content[0].vehicle_model_id
-        VehicleFuelTypeElement.value = data.data_content[0].vehicle_fuel_type_id
-        VehicleOwnerElement.value = data.data_content[0].vehicle_owner_id
-        VehicleYearElement.value = data.data_content[0].vehicle_year_manufacturer_id
-        ChassisNumberElement.value = data.data_content[0].chassis_number
-        VehicleColorElement.value = data.data_content[0].vehicle_color
+           let VehicleColorElement = document.getElementById("vehicle_color"); 
+           var buttonElement = document.getElementById("btn_edit_vehicle");
+        
+           buttonElement.setAttribute("data-id", data.data_content[0].id);
+          VehicleNumberElement.value = data.data_content[0].vehicle_number
+          EngineNumberElement.value = data.data_content[0].engine_number
+          VehicleManufacturerElement.value = data.data_content[0].vehicle_manufacturer_id
+          VehicleCountryElement.value = data.data_content[0].vehicle_country_id
+          VehicleModelElement.value = data.data_content[0].vehicle_model_id
+          VehicleFuelTypeElement.value = data.data_content[0].vehicle_fuel_type_id
+          VehicleOwnerElement.value = data.data_content[0].vehicle_owner_id
+          VehicleYearElement.value = data.data_content[0].vehicle_year_manufacturer_id
+          ChassisNumberElement.value = data.data_content[0].chassis_number
+          VehicleColorElement.value = data.data_content[0].vehicle_color
 
    
            
