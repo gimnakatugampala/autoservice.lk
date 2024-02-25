f9bf9158d06318245bccb3602d7df93a08b0bdb1
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-02-25 14:25:16 +0530

    2024_2_25_cancel_jobcard_list

---
+++ api/cancel-jobcard-list.php
@@ -0,0 +1,30 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once '../includes/db_config.php';
+
+ $stationID = $_SESSION['station_id'];
+
+$sql = "SELECT * ,
+job_card.created_date AS JOB_CARD_PLACED_DATE,
+job_card_type.type AS JOB_CARD_TYPE
+FROM job_card 
+JOIN vehicle ON job_card.vehicle_id = vehicle.id
+JOIN vehicle_owner ON job_card.vehicle_owner_id = vehicle_owner.id
+JOIN job_card_type ON job_card.job_card_type_id = job_card_type.id
+WHERE job_card.status_id = 2 AND job_card.service_station_id = '$stationID' ORDER BY job_card.created_date DESC";
+$result = $conn->query( $sql );
+
+$jobcards = array();
+
+if ( $result->num_rows > 0 ) {
+    while ( $row = $result->fetch_assoc() ) {
+        $jobcards[] = $row;
+    }
+}
+
+// echo json_encode( $result );
+?>
--- job-cards/cancel-job.php
+++ job-cards/cancel-job.php
@@ -1,5 +1,6 @@
 
 <?php include_once '../includes/header.php';?>
+<?php include_once '../api/cancel-jobcard-list.php';?>
 
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -19,18 +20,15 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-5">
-            <h1>Pending Job Card List</h1>
+          <div class="col-sm-10">
+            <h1>Cancel Job Card List</h1>
           </div>
-          <div class="col-sm-7">
+          <div class="col-sm-2">
             <!-- <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Vehicles</li>
             </ol> -->
-            <a href="../job-cards/add-repair-order.php" type="button" class="btn bg-gradient-primary"><i class="fas fa-plus"></i> Add Repair</a>
-            <a href="../job-cards/add-service-order.php" type="button" class="btn bg-gradient-primary"><i class="fas fa-plus"></i> Add Service</a>
-            <a href="../job-cards/add-service-repair-order.php" type="button" class="btn bg-gradient-primary"><i class="fas fa-plus"></i> Add Repair & Service</a>
-            <a href="../job-cards/add-wash-order.php" type="button" class="btn bg-gradient-primary"><i class="fas fa-plus"></i> Add Washer</a>
+            <a href="../job-cards/add-job-card.php" type="button" class="btn bg-gradient-primary"><i class="fas fa-plus"></i> Add Job Card</a>
           </div>
 
         </div>
@@ -47,35 +45,35 @@
             <!-- /.card -->
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Pending Job Cards</h3>
+                <h3 class="card-title">Cancel Job Cards</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
                   <thead>
                   <tr>
-                    <th>ID</th>
                     <th>Job Card Code</th>
                     <th>Vehicle Owner Name</th>
+                    <th>Phone</th>
                     <th>Vehicle Name</th>
+                    <th>Job Card Type</th>
                     <th>Created Date</th>
-                    <th>Canceled Date</th>
-                    <th>Actions</th>
                   </tr>
                   </thead>
                   <tbody>
 
-                  <tr>
-                    <td>001</td>
-                    <td>AS344</td>
-                    <td>Gimna Katugampala</td>
-                    <td>KY-3038</td>
-                    <td>2024-01-15</td>
-                    <td>2024-01-25</td>
-                    <td>
-                    <button type="button" class="btn bg-gradient-primary"><i class="fas fa-eye"></i></button>
-                    </td>
-                  </tr>
+                  <?php foreach ($jobcards as $row) : ?>
+
+                    <tr>
+                      <td><?php echo  $row["job_card_code"]; ?></td>
+                      <td><?php echo  $row["first_name"]; ?> <?php echo  $row["last_name"]; ?></td>
+                      <td><?php echo  $row["phone"]; ?></td>
+                      <td><?php echo  $row["vehicle_number"]; ?></td>
+                      <td><?php echo  $row["JOB_CARD_TYPE"]; ?></td>
+                      <td><?php echo  $row["JOB_CARD_PLACED_DATE"]; ?></td>
+                    </tr>
+
+                    <?php endforeach; ?>
                 
                   </tbody>
                 </table>
