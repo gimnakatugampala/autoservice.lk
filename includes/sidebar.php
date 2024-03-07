4dad76222ae9155615d44588a15e8fa814af059c
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-03-07 22:14:57 +0530

    2024_3_7_logo

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="../assets/img/system/autoservice_logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">autoservice.lk</span>
     </a>
 
