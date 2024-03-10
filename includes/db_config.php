aafe3669cee34258e15b1e9369a7fe44cd500ae9
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-03-10 14:44:28 +0530

    2024_3_10_cred

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,16 +1,16 @@
 <?php
 
-// LOCAL HOST SERVER
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "autoservice_db";
+// // LOCAL HOST SERVER
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "autoservice_db";
 
 // TEST SERVER
-// $servername = "localhost";
-// $username = "autoserv_root";
-// $password = "AB{A926!}Pa7";
-// $dbname = "autoserv_autoservice_test_db";
+$servername = "localhost";
+$username = "autoserv_root";
+$password = "AB{A926!}Pa7";
+$dbname = "autoserv_autoservice_test_db";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
