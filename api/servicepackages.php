972221fc437161d00181161a278c012b13345c49
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-01-25 18:07:55 +0530

    service_packages

--- api/servicepackages.php
+++ api/servicepackages.php
@@ -1,10 +1,17 @@
 <?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once '../includes/db_config.php';
 
-$sql = 'SELECT * ,
+$stationID = $_SESSION['station_id'];
+
+$sql = "SELECT * ,
 vehicle_class.name AS vehicle_class_name FROM service_packages 
 JOIN vehicle_class ON service_packages.vehicle_type_id = vehicle_class.id
-WHERE is_deleted = 0 ORDER BY created_date DESC';
+WHERE service_packages.is_deleted = 0 AND service_packages.service_station_id = '$stationID' ORDER BY created_date DESC";
 $result = $conn->query( $sql );
 
 $service_packages = array();
