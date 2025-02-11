553b61473b7a740a6cb0e87653eb55a2f05258a6
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-02-11 11:14:55 +0530

    2025_completed

--- job-cards/completed-job.php
+++ job-cards/completed-job.php
@@ -59,6 +59,7 @@
                     <th>Job Card Type</th>
                     <th>Created Date</th>
                     <th>Completed Date</th>
+                    <th>Actions</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -73,6 +74,11 @@
                       <td><?php echo  $row["JOB_CARD_TYPE"]; ?></td>
                       <td><?php echo  $row["JOB_CARD_PLACED_DATE"]; ?></td>
                       <td><?php echo  $row["COMPLETED_DATE"]; ?></td>
+                      <td> 
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-xl">
+                      <i class="fa fa-address-book" aria-hidden="true"></i>
+                      </button>
+                    </td>
                     </tr>
 
                     <?php endforeach; ?>
@@ -94,6 +100,31 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+  <!-- modal -->
+
+  <div class="modal fade" id="modal-xl">
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Extra Large Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+  <!-- modal -->
 
   <?php include_once '../includes/sub-footer.php';?>
 
