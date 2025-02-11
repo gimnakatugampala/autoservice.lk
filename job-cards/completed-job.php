7ef13790b233de4931915f6669917825a8525c27
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-02-11 11:44:49 +0530

    invoice_modal

--- job-cards/completed-job.php
+++ job-cards/completed-job.php
@@ -115,32 +115,12 @@
             <div class="modal-body">
  
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Invoice</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Invoice</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
+ 
     <section class="content">
       <div class="container-fluid">
         <div class="row">
           <div class="col-12">
-            <div class="callout callout-info">
-              <h5><i class="fas fa-info"></i> Note:</h5>
-              This page has been enhanced for printing. Click the print button at the bottom of the invoice to test.
-            </div>
-
-
+          
             <!-- Main content -->
             <div class="invoice p-3 mb-3">
               <!-- title row -->
