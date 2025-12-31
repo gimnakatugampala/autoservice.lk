d1ae718dd16be37b3cea52edf03882f625b381b8
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-12-31 23:29:21 +0530

    edit_fuel_type

--- service-packages/edit-fuel-type.php
+++ service-packages/edit-fuel-type.php
@@ -1,26 +1,33 @@
-
 <?php include_once '../includes/header.php';?>
 
-<body class="hold-transition sidebar-mini">
+<style>
+    /* AdminLTE professional standards for Edit Pages */
+    .content-wrapper { background-color: #f4f6f9; }
+    .card-info.card-outline { border-top: 3px solid #17a2b8; }
+    .form-group label { font-weight: 600; color: #495057; }
+    .text-danger { margin-left: 2px; }
+    
+    /* Input Group refinement */
+    .input-group-text {
+        background-color: #f8f9fa;
+        color: #17a2b8;
+    }
+</style>
+
+<body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <?php include_once '../includes/loader.php';?>
 
-  <!-- Navbar -->
   <?php include_once '../includes/navbar.php'; ?>
-  <!-- /.navbar -->
+  <?php include_once '../includes/sidebar.php';?>
 
-  <!-- Main Sidebar Container -->
- <?php include_once '../includes/sidebar.php';?>
-
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Edit Lubricant Type</h1>
+            <h1 class="m-0 font-weight-bold text-dark"><i class="fas fa-edit mr-2 text-info"></i>Edit Lubricant Type</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -28,69 +35,55 @@
               <li class="breadcrumb-item active">Edit Lubricant Type</li>
             </ol>
           </div>
-
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+      </div></section>
 
 
-    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-12">
+          <div class="col-md-8 mx-auto">
       
-            <!-- /.card -->
-            <div class="card">
+            <div class="card card-info card-outline shadow-sm">
+              <div class="card-header">
+                <h3 class="card-title text-bold">Update Lubricant Details</h3>
+              </div>
               <div class="card-body">
 
               <div class="row">
 
-              <div class="col-md-12">
+                <div class="col-md-12">
                     <div class="form-group">
                           <label for="fuel_type">Lubricant Type <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control" id="fuel_type" placeholder="Name">
+                          <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-flask"></i></span>
+                            </div>
+                            <input type="text" class="form-control font-weight-bold" id="fuel_type" placeholder="Name">
+                          </div>
                       </div>
                 </div>
 
-
-        
-            
-
-                <div class="col-md-4">
-                <button type="button" class="btn bg-gradient-secondary">Cancel</button>
-                <button id="btn_update_fuel" type="button" class="btn bg-gradient-primary">Update</button>
+                <div class="col-md-12 mt-3 border-top pt-3 text-right">
+                    <button type="button" class="btn btn-secondary px-4 mr-1" onclick="window.history.back();">Cancel</button>
+                    <button id="btn_update_fuel" type="button" class="btn btn-info px-5 shadow-sm">Update</button>
                 </div>
 
               </div>
 
 
               </div>
-              <!-- /.card-body -->
+              </div>
             </div>
-            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
+      </section>
+    </div>
   <?php include_once '../includes/sub-footer.php';?>
 
-  <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
+    </aside>
+  </div>
 <script src="../plugins/jquery/jquery.min.js"></script>
 <script src="../assets/js/getfueltypes.js"></script>
 
