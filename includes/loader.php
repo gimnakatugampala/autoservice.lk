c6c388da8ce4585ee5f11686ad7c797c7e0e5427
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-01-01 11:36:58 +0530

    loading

--- includes/loader.php
+++ includes/loader.php
@@ -1,4 +1,8 @@
-  <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="../assets/img/system/autoservice_logo.jpg" alt="AUTOSERVICE" height="60" width="60">
+<div class="preloader flex-column justify-content-center align-items-center">
+  <img src="../assets/img/system/autoservice_logo.jpg" alt="AUTOSERVICE" height="60" width="60" class="mb-3">
+  <div class="progress progress-xs w-25">
+    <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+    </div>
   </div>
+  <small class="text-uppercase mt-2" style="letter-spacing: 2px;">Loading System</small>
+</div>
