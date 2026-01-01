4bec9e5fc64831df7f4aa3ded3a6f8313a8f8537
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-01-01 11:19:12 +0530

    station_profile

--- station-profile/index.php
+++ station-profile/index.php
@@ -1,47 +1,40 @@
-
 <?php include_once '../includes/header.php';?>
 
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <?php include_once '../includes/loader.php';?>
 
-  <!-- Navbar -->
   <?php include_once '../includes/navbar.php'; ?>
-  <!-- /.navbar -->
+  <?php include_once '../includes/sidebar.php';?>
 
-  <!-- Main Sidebar Container -->
- <?php include_once '../includes/sidebar.php';?>
-
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Station Profile</h1>
+            <h1><i class="fas fa-gas-pump mr-2"></i>Station Profile</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Station Profile</a></li>
-              <li class="breadcrumb-item active">Profile</li>
+              <li id="breadcrumb_vehicle_number" class="breadcrumb-item active">Profile</li>
             </ol>
           </div>
 
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+      </div></section>
 
 
-    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-12">
+          <div class="col-md-10 mx-auto">
       
-            <!-- /.card -->
-            <div class="card">
+            <div class="card card-primary card-outline shadow">
+              <div class="card-header">
+                <h3 class="card-title text-bold"><i class="fas fa-info-circle mr-1"></i> Manage Station Information</h3>
+              </div>
               <div class="card-body">
 
               <div class="row">
@@ -49,7 +42,12 @@
               <div class="col-md-6">
                 <div class="form-group">
                     <label for="station_name">Station Name <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="station_name" placeholder="Enter Station Name">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="fas fa-building"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="station_name" placeholder="Enter Station Name">
+                    </div>
                   </div>
               </div>
 
@@ -71,7 +69,7 @@
                   <label>Other Phone Number</label>
                   <div class="input-group">
                     <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                      <span class="input-group-text"><i class="fas fa-phone-alt"></i></span>
                     </div>
                     <input id="other_phone_number" type="text" class="form-control" data-inputmask='"mask": "099 9999 999"' data-mask>
                   </div>
@@ -82,60 +80,53 @@
                 <div class="col-sm-12">
                 <div class="form-group">
                 <label>Address <span class="text-danger">*</span></label>
-                <textarea id="address" class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                <textarea id="address" class="form-control" rows="3" placeholder="Enter full address..."></textarea>
                 </div>
                 </div>
 
                 <div class="col-md-6">
                 <div class="form-group">
                     <label for="street">Street</label>
-                    <input type="text" class="form-control" id="street" placeholder="Enter Street">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="fas fa-road"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="street" placeholder="Enter Street">
+                    </div>
                   </div>
               </div>
               
               <div class="col-md-6">
                 <div class="form-group">
                     <label for="city">City</label>
-                    <input type="text" class="form-control" id="city" placeholder="Enter City">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="city" placeholder="Enter City">
+                    </div>
                   </div>
               </div>
               
-
-    
-    
-                <div class="col-md-12">
-                <button type="button" class="btn bg-gradient-secondary">Cancel</button>
-                <button id="update_station_info" type="button" class="btn bg-gradient-primary">Submit</button>
-                </div>
-
+              </div> </div> <div class="card-footer text-right">
+                <button type="button" class="btn btn-secondary px-4 mr-2" onclick="window.history.back();">Cancel</button>
+                <button id="update_station_info" type="button" class="btn btn-primary px-5 shadow-sm">
+                  <i class="fas fa-save mr-1"></i> Submit
+                </button>
               </div>
 
-
-              </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            </div>
           </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
+      </section>
+    </div>
   <?php include_once '../includes/sub-footer.php';?>
 
-  <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
+    </aside>
+  </div>
+<script src="../plugins/jquery/jquery.min.js"></script>
 <?php include_once '../includes/footer.php';?>
 
 </body>
