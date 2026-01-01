645e5ac126ff2e7b76ea904394ec9f1ceb3d3a17
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-01-01 11:00:08 +0530

    edit_supplier

--- suppliers/edit-supplier.php
+++ suppliers/edit-supplier.php
@@ -1,26 +1,33 @@
-
 <?php include_once '../includes/header.php';?>
 
-<body class="hold-transition sidebar-mini">
+<style>
+    /* Styling to match AdminLTE professional standards for Edit View */
+    .content-wrapper { background-color: #f4f6f9; }
+    .card-info.card-outline { border-top: 3px solid #17a2b8; }
+    .form-group label { font-weight: 600; color: #495057; }
+    .text-danger { margin-left: 2px; }
+    
+    /* Input group icon styling */
+    .input-group-text {
+        background-color: #f8f9fa;
+        color: #17a2b8;
+    }
+</style>
+
+<body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <?php include_once '../includes/loader.php';?>
 
-  <!-- Navbar -->
   <?php include_once '../includes/navbar.php'; ?>
-  <!-- /.navbar -->
+  <?php include_once '../includes/sidebar.php';?>
 
-  <!-- Main Sidebar Container -->
- <?php include_once '../includes/sidebar.php';?>
-
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Edit Supplier</h1>
+            <h1 class="m-0 font-weight-bold text-dark"><i class="fas fa-user-edit mr-2 text-info"></i>Edit Supplier</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -30,33 +37,43 @@
           </div>
 
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+      </div></section>
 
 
-    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
           <div class="col-12">
       
-            <!-- /.card -->
-            <div class="card">
+            <div class="card card-info card-outline shadow-sm">
+              <div class="card-header border-0">
+                <h3 class="card-title text-bold">Update Supplier Information</h3>
+              </div>
               <div class="card-body">
 
               <div class="row">
 
-              <div class="col-md-6">
+                <div class="col-md-6">
                     <div class="form-group">
                           <label for="first_name">First Name <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control" id="first_name" placeholder="First Name">
+                          <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" class="form-control shadow-none" id="first_name" placeholder="First Name">
+                          </div>
                       </div>
                 </div>
 
                 <div class="col-md-6">
                     <div class="form-group">
                           <label for="last_name">Last Name <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control" id="last_name" placeholder="Last Name">
+                          <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" class="form-control shadow-none" id="last_name" placeholder="Last Name">
+                          </div>
                       </div>
                 </div>
 
@@ -64,7 +81,12 @@
                 <div class="col-md-6">
                     <div class="form-group">
                           <label for="email">Email <span class="text-danger">*</span></label>
-                          <input type="email" class="form-control" id="email" placeholder="Email">
+                          <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                            </div>
+                            <input type="email" class="form-control shadow-none" id="email" placeholder="Email">
+                          </div>
                       </div>
                 </div>
 
@@ -75,7 +97,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="fas fa-phone"></i></span>
                     </div>
-                    <input id="phone_number" type="text" class="form-control" data-inputmask='"mask": "099 9999 999"' data-mask>
+                    <input id="phone_number" type="text" class="form-control shadow-none" data-inputmask='"mask": "099 9999 999"' data-mask>
                   </div>
                 </div>
                 </div>
@@ -86,9 +108,9 @@
                   <label>Other Phone Number</label>
                   <div class="input-group">
                     <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                      <span class="input-group-text"><i class="fas fa-phone-alt"></i></span>
                     </div>
-                    <input id="other_phone_number" type="text" class="form-control" data-inputmask='"mask": "099 9999 999"' data-mask>
+                    <input id="other_phone_number" type="text" class="form-control shadow-none" data-inputmask='"mask": "099 9999 999"' data-mask>
                   </div>
                 </div>
                 </div>
@@ -96,7 +118,7 @@
                 <div class="col-md-12">
                 <div class="form-group">
                         <label>Address <span class="text-danger">*</span></label>
-                        <textarea id="address" class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                        <textarea id="address" class="form-control shadow-none" rows="3" placeholder="Enter ..."></textarea>
                       </div>
                 </div>
 
@@ -105,40 +127,28 @@
 
           
 
-                <div class="col-md-4">
-                <button type="button" class="btn bg-gradient-secondary">Cancel</button>
-                <button id="btn_update_supplier" type="button" class="btn bg-gradient-primary">Update</button>
+                <div class="col-md-12 border-top pt-3 text-right">
+                    <button type="button" class="btn btn-secondary px-4 mr-1" onclick="window.history.back();">Cancel</button>
+                    <button id="btn_update_supplier" type="button" class="btn btn-info px-5 shadow-sm font-weight-bold">
+                        <i class="fas fa-save mr-1"></i> Update
+                    </button>
                 </div>
 
               </div>
 
 
               </div>
-              <!-- /.card-body -->
+              </div>
             </div>
-            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
+      </section>
+    </div>
   <?php include_once '../includes/sub-footer.php';?>
 
-  <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
+    </aside>
+  </div>
 <script src="../plugins/jquery/jquery.min.js"></script>
 <script src="../assets/js/getsuppliers.js"></script>
 
