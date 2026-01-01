91975db328dedf4a3f5093ba1171106d9ceceb4c
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-01-01 11:38:13 +0530

    footer

--- includes/sub-footer.php
+++ includes/sub-footer.php
@@ -1,6 +1,6 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b>
+<footer class="main-footer text-sm">
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; <?php echo date('Y'); ?>  <a href="https://autoservice.lk">autoservice.lk</a>.</strong> All rights reserved.
-  </footer>
+    <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="https://autoservice.lk" class="text-primary">autoservice.lk</a>.</strong> All rights reserved.
+</footer>
