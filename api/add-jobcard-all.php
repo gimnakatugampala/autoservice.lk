fa3fb27ab038c1405a32f62f35dd067df341cb29
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-01-16 10:56:30 +0530

    job_card_sms

--- api/add-jobcard-all.php
+++ api/add-jobcard-all.php
@@ -245,9 +245,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     if($status == "1"){
                         // ----------- SMS [Pending] --------------
-                        // $status_name="Pending";
-                        // $job_card_type_name="All";
-                        // include_once '../api/send-jobcard-sms.php';
+                        $status_name="Pending";
+                        $job_card_type_name="All";
+                        include_once '../api/send-jobcard-sms.php';
                         // ----------- SMS [Pending] --------------
                     }
 
@@ -376,13 +376,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
                     // ----------- SMS [Completed] --------------
-                    // $status_name="Completed";
-                    // $job_card_type_name="Repair";
-                    // include_once '../api/send-jobcard-sms.php';
+                    $status_name="Completed";
+                    $job_card_type_name="Repair";
+                    include_once '../api/send-jobcard-sms.php';
                     // ----------- SMS [Completed] --------------
 
                     // ------------------ SEND EMAIL ----------------
-                    // include_once '../api/job-card-all-pdf-maker.php';
+                    include_once '../api/job-card-all-pdf-maker.php';
                     // ------------------ SEND EMAIL ----------------
 
 
