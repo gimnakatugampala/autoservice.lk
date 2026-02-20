40a3ab1c301dabc37117277263c1b5a3cd29d5ba
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-02-20 15:17:19 +0530

    small_update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../dashboard/" class="brand-link text-center">
+    <a href="../vehicles/" class="brand-link text-center">
       <img src="../assets/img/system/autoservice_logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">autoservice.lk</span>
     </a>
